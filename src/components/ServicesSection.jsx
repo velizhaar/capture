ab17cb991bfe108994f7886022e1f162b458f8f4
velizhaar <velizhaar@gmail.com>
2021-01-27 01:17:29 +0700

    3. Services Section

---
+++ src/components/ServicesSection.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+// import icons
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <h2>
+        High <span>quality</span> services
+      </h2>
+      <div className="cards">
+        <div className="card">
+          <div className="icon">
+            <img alt="icon" src={clock} />
+            <h3>Efficient</h3>
+          </div>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div className="card">
+            <div className="icon">
+              <img alt="icon" src={diaphragm} />
+              <h3>Diaphragm </h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img alt="icon" src={money} />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img alt="icon" src={teamwork} />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="mage">
+          <img alt="camera" src={home2} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
