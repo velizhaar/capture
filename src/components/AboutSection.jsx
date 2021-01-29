c5cffacc0d81c4441f13bdf72ba9ca5f76b7c8dc
velizhaar <velizhaar@gmail.com>
2021-01-29 15:27:31 +0700

    Adding Transitions

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import home1 from "../img/home1.png";
-// Styled
-import styled from "styled-components";
+// Framer Motion
+import { motion } from "framer-motion";
 import { About, Description, Hide, Image } from "../styles";
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
-          <div className="hide">
-            <h2>We work to make</h2>
-          </div>
+        <motion.div>
           <Hide>
-            <h2>
+            <motion.h2>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professinals with amazing skills.
